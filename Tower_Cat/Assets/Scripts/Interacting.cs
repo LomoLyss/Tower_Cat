505412d4115c2865fc987344024f0ff89b674321
LomoLyss <vargasa1998@gmail.com>
2025-05-28 18:18:46 -0500

    Still fixing interactivity

--- Tower_Cat/Assets/Scripts/Interacting.cs
+++ Tower_Cat/Assets/Scripts/Interacting.cs
@@ -7,12 +7,17 @@ public class Interacting : MonoBehaviour
     [SerializeField] LayerMask LayerMask;
    // [SerializeField] GameObject Object;
     public float range = 100f;
+    public float offset = 0.01f;
+   // Vector3 Rayoffset;
+    
 
     CharacterController characterController;
     RaycastHit hit;
     
     void Update()
     {
+
+       // Rayoffset = new Vector3(0f,offset,0f);
         characterController = GetComponent<CharacterController>();
         Ray ray = new Ray(characterController.transform.position, characterController.transform.forward);
         characterController.Raycast(ray, out hit, range);
