c8b519b0a50665a2c4d64483da3088c160f53fb5
LomoLyss <vargasa1998@gmail.com>
2025-05-28 19:38:52 -0500

    System can recognize
    casting works, on button press checks if hitting an object it can interact with

--- Tower_Cat/Assets/Scripts/Interacting.cs
+++ Tower_Cat/Assets/Scripts/Interacting.cs
@@ -4,35 +4,44 @@ using UnityEngine;
 
 public class Interacting : MonoBehaviour
 {
-    [SerializeField] LayerMask LayerMask;
+    [SerializeField] LayerMask CheckingLayer;
     [SerializeField] GameObject Casting_Object;
     public float range = 100f;
     public float offset = 0.01f;
     public BoxCollider Casting_Box;
+    public Ray ray;
+    public RaycastHit hit;
     // Vector3 Rayoffset;
-    
 
-    
+
+
     //CharacterController characterController;
-    RaycastHit hit;
 
     private void Awake()
     {
         Casting_Object = gameObject;
         Casting_Box = Casting_Object.gameObject.GetComponent<BoxCollider>();
+        
     }
 
     void Update()
     {
+
         
         Ray ray = new Ray(Casting_Object.transform.position, Casting_Object.transform.forward);
-        
-
-       // Rayoffset = new Vector3(0f,offset,0f);
-       // characterController = GetComponent<CharacterController>();
-       // Ray ray = new Ray(characterController.transform.position, characterController.transform.forward);
         Casting_Box.Raycast(ray, out hit, range);
         Debug.DrawRay(ray.origin, ray.direction, Color.red);
 
+        if (Input.GetKeyDown("e"))
+        {
+            Debug.Log("Key Down");
+            if(Physics.Raycast(ray, out hit, CheckingLayer))
+            {
+                Debug.Log("Found Object");
+            }
+        }
+
     }
+
+   
 }
