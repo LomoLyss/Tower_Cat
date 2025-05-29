a317584585eb5e564a61d6f7977a22362c764564
LomoLyss <vargasa1998@gmail.com>
2025-05-28 19:16:54 -0500

    CAST WORKS
    red casting line is coming from child of player to "see" objects

--- Tower_Cat/Assets/Scripts/Interacting.cs
+++ Tower_Cat/Assets/Scripts/Interacting.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 public class Interacting : MonoBehaviour
 {
     [SerializeField] LayerMask LayerMask;
-   // [SerializeField] GameObject Object;
+    [SerializeField] GameObject Casting_Object;
     public float range = 100f;
     public float offset = 0.01f;
-   // Vector3 Rayoffset;
+    public BoxCollider Casting_Box;
+    // Vector3 Rayoffset;
     
 
-    CharacterController characterController;
-    RaycastHit hit;
     
+    //CharacterController characterController;
+    RaycastHit hit;
+
+    private void Awake()
+    {
+        Casting_Object = gameObject;
+        Casting_Box = Casting_Object.gameObject.GetComponent<BoxCollider>();
+    }
+
     void Update()
     {
+        
+        Ray ray = new Ray(Casting_Object.transform.position, Casting_Object.transform.forward);
+        
 
        // Rayoffset = new Vector3(0f,offset,0f);
-        characterController = GetComponent<CharacterController>();
-        Ray ray = new Ray(characterController.transform.position, characterController.transform.forward);
-        characterController.Raycast(ray, out hit, range);
+       // characterController = GetComponent<CharacterController>();
+       // Ray ray = new Ray(characterController.transform.position, characterController.transform.forward);
+        Casting_Box.Raycast(ray, out hit, range);
         Debug.DrawRay(ray.origin, ray.direction, Color.red);
+
     }
 }
